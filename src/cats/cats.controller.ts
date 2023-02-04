63b95016468361caf1dd74b4f2d5b846e3e68151
episohe <maybe_be@naver.com>
2023-02-04 21:50:39 +0900

    Swagger 추가 및 cors 설정

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -4,6 +4,8 @@ import { HttpExceptionFilter } from "src/common/exceptions/http-exception.filter
 import { SuccessInterceptor } from "src/common/interceptors/success.interceptor";
 import { CatsService } from "./cats.service";
 import { CatRequestDto } from "./dto/cats.request.dto";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { ReadOnlyCatDto } from "./dto/cat.dto";
 
 @Controller("cats")
 @UseInterceptors(SuccessInterceptor)
@@ -15,7 +17,12 @@ export class CatsController {
   getCurrentCat() {
     return "current cat";
   }
-
+  @ApiResponse({
+    status: 201,
+    description: "회원가입 성공",
+    type: ReadOnlyCatDto,
+  })
+  @ApiOperation({ summary: "회원가입" })
   @Post()
   async signUp(@Body() body: CatRequestDto) {
     console.log(body);
