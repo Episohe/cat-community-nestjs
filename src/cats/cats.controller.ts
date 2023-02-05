a6c77fcbc95ccde52178f0632f40591845cb9dea
episohe <maybe_be@naver.com>
2023-02-05 21:32:22 +0900

    로그인 With JWT

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,22 +1,34 @@
-import { Body, UseFilters, UseInterceptors } from "@nestjs/common";
-import { Controller, Get, Post, Put } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseFilters,
+  UseInterceptors,
+} from "@nestjs/common";
 import { HttpExceptionFilter } from "src/common/exceptions/http-exception.filter";
 import { SuccessInterceptor } from "src/common/interceptors/success.interceptor";
 import { CatsService } from "./cats.service";
 import { CatRequestDto } from "./dto/cats.request.dto";
 import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { ReadOnlyCatDto } from "./dto/cat.dto";
+import { AuthService } from "../auth/auth.service";
+import { LoginRequestDto } from "../auth/dto/login.request.dto";
 
 @Controller("cats")
 @UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
-  constructor(private readonly catsService: CatsService) {}
+  constructor(
+    private readonly catsService: CatsService,
+    private readonly authService: AuthService
+  ) {}
 
   @Get()
   getCurrentCat() {
     return "current cat";
   }
+
   @ApiResponse({
     status: 201,
     description: "회원가입 성공",
@@ -29,9 +41,10 @@ export class CatsController {
     return await this.catsService.signUp(body);
   }
 
+  @ApiOperation({ summary: "로그인" })
   @Post("login")
-  logIn() {
-    return "login";
+  logIn(@Body() data: LoginRequestDto) {
+    return this.authService.jwtLogIn(data);
   }
 
   @Post("logout")
